0cf4ff692f0661f2597861056d0ee4c5cb429c23
olegsinyavskiy <sinyavskiy@braincorporation.com>
2015-03-18 22:12:54 -0700

    simple test

---
+++ src/main/java/otognan/Person.java
@@ -0,0 +1,16 @@
+package otognan;
+
+public class Person {
+	
+	public Person(String name) {
+		this.name = name;
+	}
+	
+	
+	public String getName() {
+		return this.name;
+	}
+	
+	private String name;
+
+}
---
+++ src/test/java/TestPerson.java
@@ -0,0 +1,16 @@
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+import otognan.Person;
+
+public class TestPerson {
+
+	@Test
+	public void testName() {
+		Person person = new Person("Pete");
+		assertEquals(person.getName(), "Pete");
+	}
+
+}
