8ebcbd8ad250fe81afbcbb8d91c349aa3670c23f
olegsinyavskiy <olegsinyavskiy@gmail.com>
2015-03-21 12:40:57 -0700

    add pure controller test

--- src/test/java/otognan/TestHelloController.java
+++ src/test/java/otognan/TestHelloController.java
@@ -1,47 +1,38 @@
 package otognan;
 
 import static org.hamcrest.Matchers.equalTo;
-import static org.junit.Assert.assertThat;
-
-import java.net.URL;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.test.IntegrationTest;
 import org.springframework.boot.test.SpringApplicationConfiguration;
-import org.springframework.boot.test.TestRestTemplate;
-import org.springframework.http.ResponseEntity;
+import org.springframework.http.MediaType;
+import org.springframework.mock.web.MockServletContext;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
-import org.springframework.web.client.RestTemplate;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
 @RunWith(SpringJUnit4ClassRunner.class)
-@SpringApplicationConfiguration(classes = Application.class)
+@SpringApplicationConfiguration(classes = MockServletContext.class)
 @WebAppConfiguration
-@IntegrationTest({
-	"server.port=0",
-	"fb.client_id=0",
-	"fb.secret=0",
-	"fb.redirect_uri=null"})
 public class TestHelloController {
 
-	@Value("${local.server.port}")
-	private int port;
-	
-	private URL base;
-	private RestTemplate template;
-	
+	private MockMvc mvc;
+
 	@Before
 	public void setUp() throws Exception {
-		this.base = new URL("http://localhost:" + port + "/hello");
-		this.template = new TestRestTemplate();
+		mvc = MockMvcBuilders.standaloneSetup(new HelloController()).build();
 	}
-	
+
 	@Test
 	public void getHello() throws Exception {
-		ResponseEntity<String> response = template.getForEntity(base.toString(), String.class);
-		assertThat(response.getBody(), equalTo("Greetings from Spring Boot!"));
+		mvc.perform(MockMvcRequestBuilders.get("/hello").accept(MediaType.APPLICATION_JSON))
+				.andExpect(status().isOk())
+				.andExpect(content().string(equalTo("Greetings from Spring Boot!")));
 	}
 }
+
