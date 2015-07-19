f062294ff32d1c755323e508e4c8eb76b9771d68
olegsinyavskiy <olegsinyavskiy@gmail.com>
2015-07-19 16:48:16 -0700

    add container support

--- src/main/java/com/otognan/driverpete/security/StatelessAuthentication.java
+++ src/main/java/com/otognan/driverpete/security/StatelessAuthentication.java
@@ -3,16 +3,27 @@ package com.otognan.driverpete.security;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.social.SocialWebAutoConfiguration;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.context.web.SpringBootServletInitializer;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 @EnableAutoConfiguration(exclude = { SocialWebAutoConfiguration.class })
 @Configuration
 @ComponentScan
-public class StatelessAuthentication {
+public class StatelessAuthentication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(StatelessAuthentication.class, args);
 	}
 
+	/*
+	 * This method together with inheriting SpringBootServletInitializer makes the
+	 * application class support Servlet 3.0 interfaces so that it can be executed
+	 * in the external servlet container (e.g. Tomcat provided by Amazon)
+	*/
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(StatelessAuthentication.class);
+    }
 }
